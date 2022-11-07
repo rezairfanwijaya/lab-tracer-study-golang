27ace6155b237e802c456c2c7eabf19ab2b365c8
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-11-07 16:15:41 +0700

    update: response

--- tracer/formater.go
+++ tracer/formater.go
@@ -1,9 +1,11 @@
 package tracer
 
 type tracerFormater struct {
-	Name string `json:"name"`
-	City string `json:"city"`
-	Job  string `json:"job"`
+	Name      string `json:"name"`
+	City      string `json:"city"`
+	Job       string `json:"job"`
+	Latitude  string `json:"latitude"`
+	Longitude string `json:"longitude"`
 }
 
 func singleTracerFormater(tracer Tracer) tracerFormater {
@@ -11,6 +13,8 @@ func singleTracerFormater(tracer Tracer) tracerFormater {
 	tracerFormated.City = tracer.City
 	tracerFormated.Job = tracer.Job
 	tracerFormated.Name = tracer.Name
+	tracerFormated.Latitude = tracer.Latitude
+	tracerFormated.Longitude = tracer.Longitude
 
 	return tracerFormated
 }
