74c8c2876402a2f5e86cee4dd18286530ad005e8
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-11-07 14:29:58 +0700

    add: handler

---
+++ handler/tracer.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rezairfanwijaya/lab-tracer-study-golang/helper"
+	"github.com/rezairfanwijaya/lab-tracer-study-golang/tracer"
+)
+
+type tracerHandler struct {
+	tracerService tracer.TracerService
+}
+
+func NewTracerHandler(tracerService tracer.TracerService) *tracerHandler {
+	return &tracerHandler{tracerService}
+}
+
+func (h *tracerHandler) GetAll(c *gin.Context) {
+	tracers, err := h.tracerService.ShowAll()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, tracers)
+		return
+	}
+
+	response := tracer.TracerFormater(tracers)
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *tracerHandler) SaveTracer(c *gin.Context) {
+	// binding
+	var inputTracer tracer.TracerInput
+
+	err := c.ShouldBindJSON(&inputTracer)
+	if err != nil {
+		myError := helper.ErrorBinding(err)
+		response := helper.ResponseAPI("failed", http.StatusBadRequest, myError)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	if err := h.tracerService.Save(inputTracer); err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	response := helper.ResponseAPI("success", http.StatusOK, "success")
+	c.JSON(http.StatusOK, response)
+}
