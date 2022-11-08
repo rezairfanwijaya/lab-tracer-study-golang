9ad082833c2402f285ed3ad3ae7caed4178eb6ff
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-11-08 23:30:57 +0700

    add: query to get tracer data group by city

--- tracer/repository.go
+++ tracer/repository.go
@@ -6,6 +6,7 @@ import "gorm.io/gorm"
 type TracerRespository interface {
 	GetAll() ([]Tracer, error)
 	Create(tracer Tracer) error
+	GetTracerBaseCity() ([]TracerBaseCity, error)
 }
 
 type tracerRespository struct {
@@ -16,6 +17,7 @@ func NewTracerRepository(db *gorm.DB) *tracerRespository {
 	return &tracerRespository{db}
 }
 
+// show all tracer
 func (r *tracerRespository) GetAll() ([]Tracer, error) {
 	var tracers []Tracer
 	if err := r.db.Find(&tracers).Error; err != nil {
@@ -25,10 +27,21 @@ func (r *tracerRespository) GetAll() ([]Tracer, error) {
 	return tracers, nil
 }
 
+// save new tracer
 func (r *tracerRespository) Create(tracer Tracer) error {
 	if err := r.db.Create(&tracer).Error; err != nil {
 		return err
 	}
 
 	return nil
+}
+
+// show tracer group by city
+func (r *tracerRespository) GetTracerBaseCity() ([]TracerBaseCity, error) {
+	var tracerBaseCity []TracerBaseCity
+
+	sql := `SELECT id, city, latitude, longitude, count(city) as total from tracers GROUP by city`
+	r.db.Raw(sql).Scan(&tracerBaseCity)
+
+	return tracerBaseCity, nil
 }
