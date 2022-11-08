a1d89e661439820a8a8817c28b5ae8a2045c88ea
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-11-08 23:31:44 +0700

    add: handler to get tracer data group by city

--- handler/tracer.go
+++ handler/tracer.go
@@ -16,17 +16,21 @@ func NewTracerHandler(tracerService tracer.TracerService) *tracerHandler {
 	return &tracerHandler{tracerService}
 }
 
+// handler to show all tracer data
 func (h *tracerHandler) GetAll(c *gin.Context) {
+	// call service
 	tracers, err := h.tracerService.ShowAll()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, tracers)
 		return
 	}
 
+	// return response
 	response := tracer.TracerFormater(tracers)
 	c.JSON(http.StatusOK, response)
 }
 
+// handler to save new tracer data
 func (h *tracerHandler) SaveTracer(c *gin.Context) {
 	// binding
 	var inputTracer tracer.TracerInput
@@ -39,12 +43,29 @@ func (h *tracerHandler) SaveTracer(c *gin.Context) {
 		return
 	}
 
+	// call service
 	if err := h.tracerService.Save(inputTracer); err != nil {
 		response := helper.ResponseAPI("failed", http.StatusBadRequest, err.Error())
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
+	// return response
 	response := helper.ResponseAPI("success", http.StatusOK, "success")
 	c.JSON(http.StatusCreated, response)
+}
+
+// handler to show tracer data group by city
+func (h *tracerHandler) GetAllTracerBaseCity(c *gin.Context) {
+	// call service
+	tracerBaseCity, err := h.tracerService.ShowBaseCity()
+	if err != nil {
+		response := helper.ResponseAPI("failed", http.StatusInternalServerError, err)
+		c.JSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	// return response
+	response := helper.ResponseAPI("success", http.StatusOK, tracerBaseCity)
+	c.JSON(http.StatusOK, response)
 }
